7ba4de1975f41c00119e46286c1ae7867b467c91
Gavin Schulz <gavin@inkling.com>
2012-06-30 17:00:40 -0700

    Refactored problem 4

--- problem4.py
+++ problem4.py
@@ -3,12 +3,12 @@
 #   2-digit numbers is 9009 = 91 99.
 #   Find the largest palindrome made from the product of two 3-digit numbers.
 
-import utils
+import utils, itertools
 
 highest = 0
-for i in range(100, 1000):
-    for j in range(100, 1000):
-        if utils.is_palindrome(str(i * j)) and (i * j) > highest:
-            highest = i * j
+
+for i, j in itertools.product(range(100, 1000), repeat=2):
+    if utils.is_palindrome(str(i * j)) and (i * j) > highest:
+        highest = i * j
 
 print(highest)
